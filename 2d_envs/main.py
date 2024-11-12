4dc47d2e402c9a4cfadd32e5a2a1d8aaef361fbf
matteodalnevo <matteodn59@gmail.com>
2024-11-12 20:43:05 +0100

    data6 - expd3 - noise 0.3

--- 2d_envs/main.py
+++ 2d_envs/main.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 	parser.add_argument("--start_timesteps", default=25e3, type=int)	# Time steps initial random policy is used
 	parser.add_argument("--eval_freq", default=5e3, type=int)       	# How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int) 		# Max time steps to run environment
-	parser.add_argument("--expl_noise", default=1, type=float)    		# Std of Gaussian exploration noise
+	parser.add_argument("--expl_noise", default=0.3, type=float)    	# Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      	# Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.99, type=float)     	# Discount factor
 	parser.add_argument("--tau", default=0.005, type=float)         	# Target network update rate
