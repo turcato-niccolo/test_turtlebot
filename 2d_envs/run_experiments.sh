89614639de990807c92a2f794baef5fffbd7c750
matteodalnevo <matteodn59@gmail.com>
2024-11-13 01:17:00 +0100

    data 6 - ddpg - 0.3 noise

--- 2d_envs/run_experiments.sh
+++ 2d_envs/run_experiments.sh
@@ -4,20 +4,20 @@
 
 for ((i=0;i<10;i+=1))
 do
-    python3 main.py \
-    --policy "OurDDPG" \
-    --env "MR-env" \
-    --seed $i &
+    #python3 main.py \
+    #--policy "OurDDPG" \
+    #--env "MR-env" \
+    #--seed $i &
 
     #python3 main.py \
     #--policy "OurDDPG" \
     #--env "MR-corridor-env" \
     #--seed $i
     
-    #python3 main.py \
-    #--policy "ExpD3" \
-    #--env "MR-env" \
-    #--seed $i &
+    python3 main.py \
+    --policy "ExpD3" \
+    --env "MR-env" \
+    --seed $i &
 
     #python3 main.py \
     #--policy "ExpD3" \
