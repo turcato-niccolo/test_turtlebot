ab70d78bec1b9ff38aa9afda9e7737cffe6b3081
matteodalnevo <matteodn59@gmail.com>
2025-03-24 10:37:51 +0100

    remevoed branch for new implementation

--- algorithms/__init__.py
+++
@@ -1,6 +0,0 @@
-# algorithms/__init__.py
-
-from .ExpD3 import DDPG as ExpD3
-from .OurDDPG import DDPG as OurDDPG
-from .SAC import SAC
-from .TD3 import TD3
--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# utils/__init__.py
-
-from .replay_buffer import ReplayBuffer
-from .file_manager import create_run_folder, create_data_folders
-from .config_handler import save_config
-
